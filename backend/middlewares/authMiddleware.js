4079e31b512dc2c0a3a94d358ead1cde54a3ff54
prathamesh <prathameshjawale068@gmail.com>
2025-08-31 17:22:06 +0530

    jwt issue solve

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -19,6 +19,8 @@ const protect = asyncHandler(async (req, res, next) => {
 
       next();
     } catch (error) {
+      console.log(error);
+      
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
